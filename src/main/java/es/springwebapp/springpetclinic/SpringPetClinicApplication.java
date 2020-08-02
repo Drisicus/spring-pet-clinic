68d2fcea805c4387a4cf65f8213c2db76828b289
Andrés <aaasssccc@gmail.com>
2020-08-02 13:18:09 +0200

    Initial commit

---
+++ src/main/java/es/springwebapp/springpetclinic/SpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package es.springwebapp.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClinicApplication.class, args);
+    }
+
+}
