25e42234ac135e5136d2f924825a810f3cd8a5d3
Andrés <aaasssccc@gmail.com>
2020-08-04 20:37:08 +0200

    Add POJOs, basic model.

---
+++ src/main/java/es/springwebapp/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package es.springwebapp.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/es/springwebapp/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package es.springwebapp.springpetclinic.model;
+
+public class Vet extends Person {
+}
