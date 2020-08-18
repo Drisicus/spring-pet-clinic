eb1012c1b6736b4a488cb48a7e1b67b2cfa4d22b
Drisicus <aaasssccc@gmail.com>
2020-08-18 19:16:57 +0200

    Modify model to JPA Entities.

--- pet-clinic-data/src/main/java/es/springwebapp/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/es/springwebapp/springpetclinic/model/Pet.java
@@ -1,13 +1,37 @@
 package es.springwebapp.springpetclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -39,5 +63,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
--- pet-clinic-data/src/main/java/es/springwebapp/springpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/es/springwebapp/springpetclinic/model/Specialty.java
@@ -1,7 +1,14 @@
 package es.springwebapp.springpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
