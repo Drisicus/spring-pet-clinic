40b5a04ef8faed24cdb6b6cdd54a55ca8927761e
Drisicus <aaasssccc@gmail.com>
2020-09-24 19:56:49 +0200

    Create and Update Owner

--- pet-clinic-data/src/main/java/es/springwebapp/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/es/springwebapp/springpetclinic/model/BaseEntity.java
@@ -1,16 +1,16 @@
 package es.springwebapp.springpetclinic.model;
 
-import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import java.io.Serializable;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -22,4 +22,8 @@ public class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    public boolean isNew(){
+        return Objects.isNull(this.id);
+    }
 }
--- pet-clinic-web/src/main/java/es/springwebapp/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/es/springwebapp/springpetclinic/controllers/OwnerController.java
@@ -5,11 +5,9 @@ import es.springwebapp.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
@@ -21,7 +19,9 @@ import static org.apache.logging.log4j.util.Strings.EMPTY;
 @Controller
 public class OwnerController {
 
-    private OwnerService ownerService;
+    private static final String VIEWS_OWNER_CREATE_OR_UPDATE_FORM = "owners/createOrUpdateOwnerForm";
+
+    private final OwnerService ownerService;
 
     public OwnerController(OwnerService ownerService){
         this.ownerService = ownerService;
@@ -66,4 +66,42 @@ public class OwnerController {
         mav.addObject(owner);
         return mav;
     }
+
+    @GetMapping("/new")
+    public String initCreationForm(Model model){
+        model.addAttribute("owner", Owner.builder().build());
+        return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/new")
+    public String processCreationForm(@Validated Owner owner, BindingResult result){
+        String resultUrl;
+        if(result.hasErrors()){
+            resultUrl = VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+        } else {
+            Owner savedOwner = ownerService.save(owner);
+            resultUrl = "redirect:/owners/" + savedOwner.getId();
+        }
+        return resultUrl;
+    }
+
+    @GetMapping("/{ownerId}/edit")
+    public String initUpdateOwnerForm(@PathVariable("ownerId") long ownerId, Model model){
+        model.addAttribute("owner", ownerService.findById(ownerId));
+        return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/{ownerId}/edit")
+    public  String processUpdateOwnerForm(@Validated Owner owner, BindingResult result, @PathVariable("ownerId") long ownerId){
+        String resultUrl;
+        if(result.hasErrors()){
+            return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+        } else {
+            owner.setId(ownerId);
+            Owner savedOwner = ownerService.save(owner);
+            resultUrl = "redirect:/owners/" + savedOwner.getId();
+        }
+        return resultUrl;
+    }
+
 }
